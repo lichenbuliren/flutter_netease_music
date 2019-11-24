78dc81ef13ad2b1198d24d66e41aa1b71bd8275b
lichenbuliren <617106268@qq.com>
2019-11-24 20:09:16 +0800

    feat(home.dart): 首页未登录状态 UI 完成

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,59 +1,134 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    controller = new AnimationController(
+        duration: const Duration(milliseconds: 5000),
+        vsync: this,
+        animationBehavior: AnimationBehavior.preserve);
+    animation = Tween(begin: 80.0, end: 430.0).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reset();
+          controller.forward();
+        }
+      });
+
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose(); // 释放资源
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Container(
+      color: Colors.red,
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 500,
+            child: Center(
+              child: Stack(
+                children: <Widget>[
+                  Center(
+                    child: Container(
+                      width: animation.value,
+                      height: animation.value,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border:
+                              Border.all(width: 1, color: Colors.redAccent)),
+                      child: null,
+                    ),
+                  ),
+                  Center(
+                    // 裁剪圆形
+                    child: ClipOval(
+                      child: new Image.asset(
+                        'assets/images/logo.png',
+                        fit: BoxFit.cover,
+                        width: 80,
+                        height: 80,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+          ),
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                      child: RaisedButton(
+                          textColor: Colors.red,
+                          color: Colors.white,
+                          onPressed: () => print('手机登录'),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(40),
+                            child: Container(
+                                width: 300,
+                                child: Center(
+                                  child: Text('手机登录'),
+                                )),
+                          )),
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                      child: RaisedButton(
+                          textColor: Colors.white,
+                          color: Colors.transparent,
+                          // 阴影偏移量
+                          elevation: 0,
+                          onPressed: () => print('立即体验'),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              side: BorderSide(width: 1, color: Colors.white)),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(40),
+                            child: Container(
+                                width: 300,
+                                child: Center(
+                                  child: Text('立即体验'),
+                                )),
+                          )),
+                    )
+                  ],
+                )
+              ],
             ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/route/route.dart
+++ lib/route/route.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:flutter_netease_music/pages/home.dart';
 
 final Map<String, Widget Function(BuildContext)> routes = {
-  '/home': (context) => MyHomePage(title: '网易云音乐'),
+  '/home': (context) => MyHomePage(),
 };
