fe6ae7c3126c236968240789ca8fb5701e46ba0e
lichenbuliren <617106268@qq.com>
2019-12-04 20:09:56 +0800

    feat(toast): 更新 toast 组件

--- lib/pages/home/index.dart
+++ lib/pages/home/index.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_netease_music/pages/home/animate_circle.dart';
 import 'package:flutter_netease_music/pages/home/icon_circle.dart';
 import 'package:flutter_netease_music/pages/home/radio_select.dart';
-import 'package:toast/toast.dart';
+import 'package:oktoast/oktoast.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -21,12 +21,11 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   }
 
   void showWarning() {
-    Toast.show(
+    showToast(
       "请先勾选《服务条款》《隐私政策》《儿童隐私政策》",
-      context,
-      duration: Toast.LENGTH_LONG,
-      gravity: Toast.CENTER,
-      backgroundRadius: 10,
+      textPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      backgroundColor: Colors.black87,
+      radius: 6,
     );
   }
 
@@ -36,109 +35,112 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     final TextStyle textStyle2 = textStyle.merge(
       TextStyle(color: Colors.white),
     );
-    return Scaffold(
-      body: Container(
-        color: Colors.red,
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 600,
-              child: Center(
-                child: Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: <Widget>[
-                    AnimatedCircle(),
-                    AnimatedCircle(delay: 2000),
-                    ClipOval(
-                      child: Image.asset(
-                        'assets/images/logo.png',
-                        fit: BoxFit.cover,
-                        width: 80,
-                        height: 80,
+    return OKToast(
+      textAlign: TextAlign.left,
+      child: Scaffold(
+        body: Container(
+          color: Colors.red,
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 600,
+                child: Center(
+                  child: Stack(
+                    alignment: AlignmentDirectional.center,
+                    children: <Widget>[
+                      AnimatedCircle(),
+                      AnimatedCircle(delay: 2000),
+                      ClipOval(
+                        child: Image.asset(
+                          'assets/images/logo.png',
+                          fit: BoxFit.cover,
+                          width: 80,
+                          height: 80,
+                        ),
                       ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                            child: RaisedButton(
+                              textColor: Colors.red,
+                              color: Colors.white,
+                              onPressed: () {
+                                handlePhoneLogin();
+                              },
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Text('手机号登录'),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                            child: RaisedButton(
+                              textColor: Colors.white,
+                              color: Colors.transparent,
+                              // 阴影偏移量
+                              elevation: 0,
+                              onPressed: () => print('立即体验'),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                side: BorderSide(width: 1, color: Colors.white),
+                              ),
+                              child: Text('立即体验'),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          IconCircle(path: 'assets/images/weixin.svg'),
+                          IconCircle(path: 'assets/images/qq.svg'),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(right: 4),
+                          child: RadioSelect(
+                            onChanged: (val) {
+                              setState(() {
+                                _agreement = !_agreement;
+                              });
+                            },
+                            value: _agreement,
+                          ),
+                        ),
+                        Text('同意', style: textStyle),
+                        Text('《服务条款》', style: textStyle2),
+                        Text('《隐私政策》', style: textStyle2),
+                        Text('《儿童隐私政策》', style: textStyle2),
+                      ],
                     ),
                   ],
                 ),
               ),
-            ),
-            Expanded(
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Container(
-                          padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
-                          child: RaisedButton(
-                            textColor: Colors.red,
-                            color: Colors.white,
-                            onPressed: () {
-                              handlePhoneLogin();
-                            },
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Text('手机号登录'),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Container(
-                          padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
-                          child: RaisedButton(
-                            textColor: Colors.white,
-                            color: Colors.transparent,
-                            // 阴影偏移量
-                            elevation: 0,
-                            onPressed: () => print('立即体验'),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                              side: BorderSide(width: 1, color: Colors.white),
-                            ),
-                            child: Text('立即体验'),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        IconCircle(path: 'assets/images/weixin.svg'),
-                        IconCircle(path: 'assets/images/qq.svg'),
-                      ],
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(right: 4),
-                        child: RadioSelect(
-                          onChanged: (val) {
-                            setState(() {
-                              _agreement = !_agreement;
-                            });
-                          },
-                          value: _agreement,
-                        ),
-                      ),
-                      Text('同意', style: textStyle),
-                      Text('《服务条款》', style: textStyle2),
-                      Text('《隐私政策》', style: textStyle2),
-                      Text('《儿童隐私政策》', style: textStyle2),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
