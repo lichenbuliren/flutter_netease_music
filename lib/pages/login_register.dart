c6e83fa03ce65e8bab8346e43d9da111bd6769eb
lichenbuliren <617106268@qq.com>
2019-12-11 21:45:29 +0800

    docs(doc): 更新文档

--- lib/pages/login_register.dart
+++ lib/pages/login_register.dart
@@ -1,4 +1,7 @@
+import 'dart:ui' as prefix0;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginRegisterPage extends StatefulWidget {
   LoginRegisterPage({Key key}) : super(key: key);
@@ -22,19 +25,93 @@ class _LoginRegisterPageState extends State<LoginRegisterPage> {
       ),
       backgroundColor: Colors.white,
       body: Padding(
-        padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+        padding: EdgeInsets.fromLTRB(20, 30, 20, 0),
         child: Column(
           children: <Widget>[
-            Text(
-              '未注册手机号登录后将自动创建账号',
-              style: new TextStyle(
-                color: Color(0xFF999999),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                '未注册手机号登录后将自动创建账号',
+                textAlign: TextAlign.left,
+                style: new TextStyle(
+                  color: Color(0xFFAAAAAA),
+                  fontSize: 16,
+                ),
               ),
             ),
-            Text(
-              'asf',
-              style: new TextStyle(
-                color: Color(0xFF999999),
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+              margin: EdgeInsets.only(bottom: 40),
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    width: 0.5,
+                    color: Color(0xFFBBBBBBB),
+                  ),
+                ),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    '+86',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 26,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: TextField(
+                        onChanged: (v) {
+                          print(v);
+                        },
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                        cursorColor: Color(0xFF777777).withOpacity(0.8),
+                        keyboardType: TextInputType.phone,
+                        textInputAction: TextInputAction.done,
+                        decoration: InputDecoration(
+                          enabledBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          border: InputBorder.none,
+                          focusColor: Colors.black54,
+                          hintText: '输入手机号',
+                          hintStyle: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 300,
+              decoration: BoxDecoration(
+                color: Colors.purple,
+                gradient: LinearGradient(
+                  colors: [Colors.lightBlue, Colors.blue],
+                  stops: [0.1, 1],
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(30),
+                ),
+              ),
+              child: RaisedButton(
+                textColor: Colors.white,
+                color: Colors.transparent,
+                // 阴影偏移量
+                elevation: 0,
+                onPressed: () => print('下一步'),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  side: BorderSide.none,
+                ),
+                child: Text('下一步'),
               ),
             ),
           ],
